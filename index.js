25b674c67bba67ccac9e25fe31176e34538309b4
Mahshid Jafarzadeh <99116494+MahshidJafarzadeh@users.noreply.github.com>
2024-08-29 18:58:33 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ import jsonServer from "json-server";
 import auth from "json-server-auth";
 
 const server = express();
+
+// تنظیمات CORS با هدرهای مشخص
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-})
+    res.header('Access-Control-Allow-Origin', '*'); // یا دامنه‌ی خاص خود را به جای * قرار دهید
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 const router = jsonServer.router('./data/db.json');
 server.use('/api', router);
-server.db = router.db
+server.db = router.db;
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 const rules = auth.rewriter({
     products: 444,
     featured_products: 444,
@@ -21,9 +29,12 @@ const rules = auth.rewriter({
     users: 600
 });
 
-server.use(rules)
-server.use(auth)
-server.use(middlewares)
-server.use(router)
+server.use(rules);
+server.use(auth);
+server.use(middlewares);
+server.use(router);
 
-server.listen(8000);
+server.listen(8000, () => {
+    console.log('JSON Server is running on port 8000');
+});
+
